bf89ada86327f1cdf35e42506f55b98b343edb3d
Eric Flumerfelt <eflumerf@fnal.gov>
2025-05-22 14:28:25 -0500

    Extend unit test coverage by checking for exceptions from bad connection strings while calling connect_for_*.

--- unittest/ZmqReceiver_test.cxx
+++ unittest/ZmqReceiver_test.cxx
@@ -30,20 +30,34 @@ BOOST_AUTO_TEST_CASE(BasicTests)
 
 BOOST_AUTO_TEST_CASE(Subscribe)
 {
-
   auto the_receiver = make_ipm_subscriber("ZmqReceiver");
   BOOST_REQUIRE(the_receiver == nullptr);
 }
 
-BOOST_AUTO_TEST_CASE(Exceptions)
+BOOST_AUTO_TEST_CASE(Errors)
 {
   auto the_receiver = make_ipm_receiver("ZmqReceiver");
   BOOST_REQUIRE(the_receiver != nullptr);
+  BOOST_REQUIRE(!the_receiver->can_receive());
 
   nlohmann::json config_json;
-  config_json["connection_string"] = "invalid_connection_string";
-  BOOST_REQUIRE_EXCEPTION(
-    the_receiver->connect_for_receives(config_json), ZmqOperationError, [&](ZmqOperationError const&) { return true; });
-}
 
+  config_json["connection_string"] = "not a uri";
+  BOOST_REQUIRE_EXCEPTION(the_receiver->connect_for_receives(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("invalid URI") != std::string::npos;
+  });
+  BOOST_REQUIRE(!the_receiver->can_receive());
+
+  config_json["connection_string"] = "tcp://thishostddoesnotexist";
+  BOOST_REQUIRE_EXCEPTION(the_receiver->connect_for_receives(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("Unable to resolve connection_string") != std::string::npos;
+  });
+  BOOST_REQUIRE(!the_receiver->can_receive());
+
+  config_json["connection_string"] = "badproto://default";
+  BOOST_REQUIRE_EXCEPTION(the_receiver->connect_for_receives(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("while calling bind on the ZMQ receive socket") != std::string::npos;
+  });
+  BOOST_REQUIRE(!the_receiver->can_receive());
+}
 BOOST_AUTO_TEST_SUITE_END()
--- unittest/ZmqSender_test.cxx
+++ unittest/ZmqSender_test.cxx
@@ -27,15 +27,30 @@ BOOST_AUTO_TEST_CASE(BasicTests)
   BOOST_REQUIRE(!the_sender->can_send());
 }
 
-BOOST_AUTO_TEST_CASE(Exceptions)
+BOOST_AUTO_TEST_CASE(Errors)
 {
   auto the_sender = make_ipm_sender("ZmqSender");
   BOOST_REQUIRE(the_sender != nullptr);
+  BOOST_REQUIRE(!the_sender->can_send());
 
   nlohmann::json config_json;
-  config_json["connection_string"] = "invalid_connection_string";
-  BOOST_REQUIRE_EXCEPTION(
-    the_sender->connect_for_sends(config_json), ZmqOperationError, [](const ZmqOperationError&) { return true; });
+
+  config_json["connection_string"] = "not a uri";
+  BOOST_REQUIRE_EXCEPTION(the_sender->connect_for_sends(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("Operation failed for all resolved connection strings") != std::string::npos;
+  });
+  BOOST_REQUIRE(!the_sender->can_send());
+  
+  config_json["connection_string"] = "tcp://thishostddoesnotexist";
+  BOOST_REQUIRE_EXCEPTION(the_sender->connect_for_sends(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("Operation failed for all resolved connection strings") != std::string::npos;
+  });
+  BOOST_REQUIRE(!the_sender->can_send());
+  
+  config_json["connection_string"] = "badproto://default";
+  BOOST_REQUIRE_EXCEPTION(the_sender->connect_for_sends(config_json), ZmqOperationError, [&](ZmqOperationError e) {
+    return std::string(e.what()).find("Operation failed for all resolved connection strings") != std::string::npos;
+  });
   BOOST_REQUIRE(!the_sender->can_send());
 }
 BOOST_AUTO_TEST_SUITE_END()
